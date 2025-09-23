79f62f296b550dfab92bdcfccc354f9868b30f80
joker09245 <jokergamerjokergamer0@gmail.com>
2025-09-23 12:29:48 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = ("MTQwNDEzNTkwNzQ5NzQ3NjE4OQ.GVCdH-.K8jQLSWoOxoW2jLJf9lyM_s_MCOje8coymsBM8")
+TOKEN = os.environ.get("TOKEN")
 NAME = "Axon X"
 server = "https://discord.com/invite/codexdev"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
